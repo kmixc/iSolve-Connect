2b9af9aade09b38b619545ae4210a8b302faa5c7
John Michael De Guzman <jmxdg1029@gmail.com>
2022-04-02 19:38:31 -0400

    Login Authentication implemented

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
 import Logo from '../img/logo.svg'
 
+import {
+    BrowserRouter as Router,
+    useHistory,
+} from "react-router-dom";
+
 export default function Login() {
+
+    const[input, setInput] = useState({
+        email: '',
+        password: ''
+    })
+
+
+function handleChange(event){
+    const {name, value} = event.target;
+
+    setInput(prevInput => {
+        return {
+            ...prevInput,
+            [name]:value
+        }
+    })
+}
+const history = useHistory();
+
+function handleClick(event){ 
+    
+    let path = `/about`
+    
+    event.preventDefault();
+    const userCheck = {
+        email: input.email,
+        password: input.password,  
+    }
+
+   
+
+    //calls the server.js http
+    axios.post('http://localhost:8082/userLogin', userCheck)
+    console.log('pressed!')
+
+    history.push(path)
+    
+}
     return (
         <div className='d-flex align-items-center justify-content-center text-light my-5 login-cont'>
             <div className='logo-form'>
@@ -12,15 +56,15 @@ export default function Login() {
                 <form className='form-login'>
                     <div class="form-group">
                         <label for="email">Email address</label>
-                        <input type="email" class="form-control" id="email" />
+                        <input onChange={handleChange} type="email" name="email" value={input.email} class="form-control" id="email" />
                     </div>
 
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" />
+                        <input onChange={handleChange} type="password" name="password" value={input.password}  class="form-control" id="password" />
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <button onClick={handleClick} type="submit" class="btn btn-primary">Login</button>
                 </form>
             </div>
         </div>
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,8 +2,9 @@ const express = require('express');
 const User = require('../user')
 const app = express();
 const router = express.Router();
+const jwt = require('jsonwebtoken')
 
-
+//Register User
 router.route("/create").post((req,res) => {
    const email = req.body.email;
    const name = req.body.name;
@@ -22,15 +23,36 @@ router.route("/create").post((req,res) => {
     newComp.save();
 })
 
+//List out Programmers
 router.route("/users").get((req,res) => {
     User.find({occupation: 'I am Programmer'})
         .then(foundUser => res.json(foundUser))
 })
 
+//List out Companies
 router.route("/company").get((req,res) => {
     User.find({occupation: 'I am Company'})
         .then(foundUser => res.json(foundUser))
 })
+
+//Login
+router.route("/userLogin").post((req,res) => {
+
+    const user = User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    }, function(err, result) {
+        const token = jwt.sign({
+            name: user.name,
+            email: user.email
+        }, 'secret')
+        res.json({status: 'ok', user: token})
+        if(!result){
+            console.log('false')
+        }
+    })
+})
+
 
 
 
